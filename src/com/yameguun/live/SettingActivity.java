afb6f558de69130cb04ead8a81e7aeaceccd3725
Seiji Tanaka <s_tanaka@system-audit.co.jp>
2014-06-18 18:37:18 +0900

    カラーピッカーの追加

--- src/com/yameguun/live/SettingActivity.java
+++ src/com/yameguun/live/SettingActivity.java
@@ -1,15 +1,51 @@
 package com.yameguun.live;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+
+import com.yameguun.live.dialog.ColorPickerDialog;
 
 public class SettingActivity extends Activity {
-	
+
+	// カラーピッカーダイアログ
+	private ColorPickerDialog mColorPickerDialog;
+
+	private SharedPreferences sharedPref;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		 setContentView(R.layout.setting_layout);
+
+		sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+
+		setContentView(R.layout.setting_layout);
+
+		mColorPickerDialog = new ColorPickerDialog(this,
+				new ColorPickerDialog.OnColorChangedListener() {
+					@Override
+					public void colorChanged(int color) {
+						Editor editor = sharedPref.edit();
+						editor.putInt("bColor", color);
+						editor.commit();
+					}
+				}, Color.BLACK);
+
+		// 背景色の設定
+		LinearLayout setcolor = (LinearLayout) findViewById(R.id.setcolor);
+		setcolor.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mColorPickerDialog.show(); // カラーピッカーダイアログを表示する
+			}
+		});
+
 	}
-	
+
 }
